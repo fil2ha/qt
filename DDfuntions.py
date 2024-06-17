15b6cdb5d31c967e1c37be9dff07d645292470d2
igr1k <igr1ks@yandex.ru>
2024-06-17 14:03:47 +0300

    17/06/2024

--- DDfuntions.py
+++ DDfuntions.py
@@ -5,15 +5,62 @@ class DataBase():
         self.connection = sl.connect('srm.db', check_same_thread=False)
         self.cursor = self.connection.cursor()
 
+    #транзакция по id
     def get_transaction_by_id(self, id):
         self.cursor.execute("SELECT * FROM Transactions WHERE id = ?", (id,))
         return self.cursor.fetchall()
 
+    #товары + коли-во
+    def get_goods(self):
+        self.cursor.execute('''
+            SELECT Goods.articul, Goods.name, Goods.price, Goods.ex_time,  GoodsWarehouse.count FROM Goods
+            JOIN GoodsWarehouse ON Goods.id = GoodsWarehouse.good_id
+        ''')
+        return self.cursor.fetchall()
+    # возвращает список картежей
+    # последний элемент кортежа количество
+
+    #товары + склад + кол-во
+    def get_goods_with_wh(self):
+        self.cursor.execute('''
+            SELECT Goods.articul, Goods.name, Goods.price, Goods.ex_time, GoodsWarehouse.count, Warehouse.name FROM Goods
+            JOIN GoodsWarehouse ON Goods.id = GoodsWarehouse.good_id
+            JOIN WareHouse ON WareHouse.id = GoodsWarehouse.warehouse_id
+        ''')
+        return self.cursor.fetchall()
+    #возвращает список картежей
+    #последний элемент кортежа имя склада
+
+    # функции дял уменьшения и увеличения кол-ва на складе определённого товара
+    def decrease_cnt(self, name, ex_time, cnt):
+        self.cursor.execute(f"""
+                                UPDATE GoodsWarehouse 
+                                SET count = count - ?
+                                WHERE id IN (
+                                SELECT id FROM Goods 
+                                WHERE name = ? AND ex_time = ?
+                            )""", (cnt, name, ex_time))
+        self.connection.commit()
+    def increase_cnt(self, name, ex_time, cnt):
+        self.cursor.execute(f"""
+                                    UPDATE GoodsWarehouse 
+                                    SET count = count + ?
+                                    WHERE id IN (
+                                    SELECT id FROM Goods 
+                                    WHERE name = ? AND ex_time = ?
+                                )""", (cnt, name, ex_time))
+        self.connection.commit()
+
+    #функция для добавления транзакции
+    def insert_transact(self):
+        pass
 
 db = DataBase()
 
-trans = db.get_transaction_by_id('1')
 
-print(trans)
+
+
+
+
 
 
