c4061982171de19a1b41a90ce0df4d10883a6e11
igr1k <igr1ks@yandex.ru>
2024-06-19 10:35:43 +0300

    17/06/2024

--- DBfuntions.py
+++ DBfuntions.py
@@ -51,7 +51,7 @@ class DataBase():
     # последний элемент кортежа имя склада
 
     # функции дял уменьшения и увеличения кол-ва на складе определённого товара
-    def decrease_cnt(self, name, ex_time, cnt):#исправить
+    def decrease_cnt(self, name, ex_time, cnt):
         self.cursor.execute(f"""
                                 UPDATE GoodsWarehouse 
                                 SET count = count - ? 
@@ -100,6 +100,45 @@ class DataBase():
         self.cursor.execute("INSERT INTO SellData (good_id, sell_id, count, expire_date) VALUES (?, ?, ?, ?)",
                             (temp_good_id, sd_list[2], sd_list[3], sd_list[4]))
         self.connection.commit()
+
+    def insert_accept(self, list_a):
+        self.cursor.execute("INSERT INTO acceptance (transaction_id, to_wh, Client) VALUES(?, ?, ?)", list_a)
+        self.connection.commit()
+        return self.cursor.lastrowid
+
+    def insert_accData(self, list_ad):
+        self.cursor.execute("SELECT id FROM Goods WHERE name = ? AND ex_time = ?", (list_ad[0], list_ad[1]))
+        temp_good_id = self.cursor.fetchone()[0]
+        self.cursor.execute("INSERT INTO AcceptanceData (good_id, acceptance_id, count, expire_date) VALUES (?, ?, ?, ?)",
+                            (temp_good_id, list_ad[2], list_ad[3], list_ad[4]))
+        self.connection.commit()
+
+    def insert_wo(self, list_wo):
+        self.cursor.execute("INSERT INTO WriteOff (transaction_id, from_wh) VALUES(?, ?)", list_wo)
+        self.connection.commit()
+        return self.cursor.lastrowid
+
+    def insert_wod(self, list_wod):
+        self.cursor.execute("SELECT id FROM Goods WHERE name = ? AND ex_time = ?", (list_wod[0], list_wod[1]))
+        temp_good_id = self.cursor.fetchone()[0]
+        self.cursor.execute(
+            "INSERT INTO WritOffData (good_id, acceptance_id, count, expire_date) VALUES (?, ?, ?, ?)",
+            (temp_good_id, list_wod[2], list_wod[3], list_wod[4]))
+        self.connection.commit()
+
+    def insert_trans(self, list_t):
+        self.cursor.execute("INSERT INTO Transportation (transaction_id, from_wh, to_wh) VALUES(?, ?, ?)", list_t)
+        self.connection.commit()
+        return self.cursor.lastrowid
+
+    def insert_transData(self, list_td):
+        self.cursor.execute("SELECT id FROM Goods WHERE name = ? AND ex_time = ?", (list_td[0], list_td[1]))
+        temp_good_id = self.cursor.fetchone()[0]
+        self.cursor.execute(
+            "INSERT INTO TransportationData (good_id, transportation_id, count, expire_date) VALUES (?, ?, ?, ?)",
+            (temp_good_id, list_td[2], list_td[3], list_td[4]))
+        self.connection.commit()
+
     def get_clients(self):
         self.cursor.execute("SELECT FIO FROM Client")
         temp = []
@@ -108,9 +147,10 @@ class DataBase():
                 temp.append(__)
         return temp
 
+
+
 db = DataBase()
 
-db.get_clients()
 
 
 
